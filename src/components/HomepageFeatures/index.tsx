7c7d6bd2c8a473f972ac4606225465e31b6c9b4c
Ashp116 <42877289+Ashp116@users.noreply.github.com>
2023-12-27 23:47:32 -0500

    Update

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,48 +4,45 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg?: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Safety',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+          Robotics safety demands PPE, lockout/tagout, regular inspections, and comprehensive training for personnel and machinery protection.
       </>
     ),
   },
+    {
+        title: 'Intro to Github',
+        description: (
+            <>
+                To learn more about Git and Github. Refer to his <a href={"https://www.youtube.com/watch?v=iv8rSLsi1xo"}>video</a>
+            </>
+        ),
+    },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Quick Links',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+          <a href={"https://github.com/BenzeneBots"}>Benzenebots Github</a>
+          <hr></hr>
+          <a href={"https://benzenebots.com/"}>Benzenebots Site</a>
       </>
     ),
   },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
